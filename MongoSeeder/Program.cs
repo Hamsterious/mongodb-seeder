222fc870051d65f629fa73a2cff22a87103eac79
martin_hammer <martin_hammer@live.dk>
2017-04-18 10:14:13 +0200

    Add project files.

---
+++ MongoSeeder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoSeeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
